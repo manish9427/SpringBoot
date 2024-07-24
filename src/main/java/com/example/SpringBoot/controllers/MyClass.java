6de3fef1e9ce0d414708fc5f4c92ecfda3514cf5
manish9427 <manish119427@gmail.com>
2024-07-24 23:46:15 +0530

    mnew

--- src/main/java/com/example/SpringBoot/controllers/MyClass.java
+++ src/main/java/com/example/SpringBoot/controllers/MyClass.java
@@ -1,10 +1,12 @@
-package com.example.SpringBoot.controller;
+package com.example.SpringBoot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyClass {
+//    @GetMapping(path="/hello")
+//    @GetMapping("/hello")
     @GetMapping("hello")
     public String sayHello(){
         return "Hello World";
---
+++ src/main/java/com/example/SpringBoot/controllers/TodoController.java
@@ -0,0 +1,6 @@
+package com.example.SpringBoot.controllers;
+
+import java.util.List;
+public class TodoController {
+
+}
