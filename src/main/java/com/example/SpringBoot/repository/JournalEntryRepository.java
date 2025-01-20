c79c507711c6f066b6634a453d73523fadff8c63
manish9427 <manish119427@gmail.com>
2025-01-20 11:19:51 +0530

    new

--- src/main/java/com/example/SpringBoot/repository/JournalEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringBoot.repository;
-
-import com.example.SpringBoot.entity.JournalEntry;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
-}
