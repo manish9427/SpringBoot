8e635329483c94063a21005e1521fd11a36f8250
manish9427 <manish119427@gmail.com>
2025-01-20 17:15:59 +0530

    new

--- src/main/java/com/example/SpringBoot/controller/Calculator.java
+++ src/main/java/com/example/SpringBoot/controller/Calculator.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class Addition {
+public class Calculator {
     @GetMapping("/add")
     @ResponseBody
     public int add(@RequestParam int a,@RequestParam int b){
