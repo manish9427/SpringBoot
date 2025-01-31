6a9f75c1c5696a85b67f623c63c574c60f89a265
manish9427 <manish119427@gmail.com>
2025-01-31 16:12:50 +0530

    Person

--- src/main/java/com/example/SpringBoot/controller/PersonName.java
+++ src/main/java/com/example/SpringBoot/controller/PersonName.java
@@ -1,10 +1,15 @@
 package com.example.SpringBoot.controller;
 //package com.example.SpringBoot.controller.Person;
 
+import java.util.Scanner;
+
 public class PersonName {
     public static void main (String args[]){
-        String name = "1234";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Name: ");
+        String name = sc.nextLine();
         Person person = new Person(name);
+        System.out.println("Output");
         person.greet();
     }
 }
