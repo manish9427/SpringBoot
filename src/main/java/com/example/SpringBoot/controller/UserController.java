bf0f3676e46001a785e2173d74e6cad13d41785b
manish9427 <manish119427@gmail.com>
2025-03-05 16:25:07 +0530

    user

---
+++ src/main/java/com/example/SpringBoot/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.SpringBoot.controller;
+
+import com.example.SpringBoot.model.User;
+import com.example.SpringBoot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return "User deleted successfully!";
+    }
+}
---
+++ src/main/java/com/example/SpringBoot/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.SpringBoot.repository;
+
+import com.example.SpringBoot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
